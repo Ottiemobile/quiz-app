9840170001c07f35a040cc7f9a7564c51f376022
Ethan Torres <torresethan99@gmail.com>
2021-01-10 08:02:57 -0600

    first commit into github

---
+++ assets/js/questions.js
@@ -0,0 +1,68 @@
+// the following questions will be shown on the quiz
+var questions = [
+    {
+        title: "which of the following is NOT used in coding websites?",
+        choices: ["javascript","CSS","HTML","Java"],
+        ans: "Java"
+    },
+
+    {
+        title: "which of the following is used to set up the structure of a website",
+        choices: ["HTML","CSS","Javascript","Python"],
+        ans: "HTML"
+    },
+
+    {
+        title: "which of the following is used to design our website?",
+        choices: ["HTML","CSS","Javascript","Groovy"],
+        ans: "CSS"
+    },
+
+    {
+        title: "which of the following helps add functionality to our website?",
+        choices: ["HTML","CSS","Javascript","PHP"],
+        ans: "Javascript"
+    },
+
+    {
+        title: "what is one of the things that Git Used for?",
+        choices: ["keeps track of versions of source code","codes our websites","can send information from one computer to another locally","all of the above"],
+        ans: "keeps track of certain versions of source code"
+    },
+
+    {
+        title: "what can we use to send our source code as repositories into the cloud?",
+        choices: ["JQuery","Github","SQL","Javascript"],
+        ans: "Github"
+    },
+
+    {
+        title: "what are issues?",
+        choices: ["problems with source code","notes that keep track of what needs to be done","conflict amongst two developers","problems with the network"],
+        ans: "notes that keep track of what needs to be done"
+    },
+
+    {
+        title: "what can we use to store data locally?",
+        choices: ["localStorage","saveData","storeDataLocally","none. all info is saved online."],
+        ans: "localStorage"
+    },
+
+    {
+        title: "what is this quiz about?",
+        choices: ["web development","Game Design","mobile data","Javascript"],
+        ans: "web development"
+    },
+
+    {
+        title: "which of the following is an example item that we can use to apply a header to our website?",
+        choices: ["<p></p>","<title></title>","<head></head>","<h1></h1>"],
+        ans: "<h1></h1>"
+    },
+
+    {
+        title: "what is one thing that HTML, CSS, and Javascript have in common",
+        choices: ["they all use the internet","they all require a specific programming program","they're all case sensitive","all of the above"],
+        ans: "they're all case sensitive"
+    }
+];
