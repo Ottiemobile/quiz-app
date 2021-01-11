e98e9c8ae405a0a840bc7170217d98263519b0b7
Ethan Torres <torresethan99@gmail.com>
2021-01-11 15:43:14 -0600

    setup for function input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
+/************************************************************************************************
+*                                                                                               *
+*                                           CODEQUIZ 2                                          *
+*                                                                                               *
+*                                   Developed by Ethan Torres                                   *
+*                                                                                               *
+*                                                                                               *
+*                                                                                               *
+*                                                                                               *
+*                                         © DevDelta 2021                                       *
+*************************************************************************************************/
+
+
+
+
+// ------------------ Global Variables ----------------------------------------------------------
 
 // Start Variable
 var start = document.getElementById("start-quiz");
@@ -19,10 +35,59 @@ var timeRemaining = (questions.length * 30 + 1);
 var scoreUser = document.getElementById("score-user");
 var scoreRemaining = document.getElementById("#score-submit")
 
-// Miscillaneous Variables
+// Miscellaneous Variables
 var submit = document.querySelector("button.submit");
 var username; 
+// ----------------------------------------------------------------------------------------------
 
 
 
-start.addEventListener("click", start);
+
+
+// ---------------------------------------- FUNCTIONS -------------------------------------------
+
+// Start quiz Function
+function start() {
+    var homepageEl = document.getElementById('homepage');
+    questionEl.removeAttribute("class"); // removes the main screen display once the button was clicked 
+
+    timerId = setInterval(timer, 5000); // sets a timer
+
+    time.textContent = timeRemaining; // displays timer
+
+    pullQuestion(); // once the quiz starts, the function pulls out a question.
+}
+// ----------------------------------------------------------------------------------------------
+
+
+//  Stop quiz Function
+function stop() {
+    clearInterval(timerId);
+
+}
+// ---------------------------------------------------------------------------------------------
+
+// Timer Function
+function timer() {
+    timeRemaining--; // deducts time every second
+    time.textContent = timeRemaining; // displays timer on quiz
+
+    if (timeRemaining <= 0) {
+        stop(); // stop sthe quiz once the timer reaches at or below 0
+    }
+}
+// ---------------------------------------------------------------------------------------------
+
+
+
+// Score Function
+function currentScore() {
+
+}
+// ----------------------------------------------------------------------------------------------
+
+// Leaderbord Function
+
+
+
+start.addEventListener("click", start); // event listener for the start function
